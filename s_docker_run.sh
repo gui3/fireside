a16509d5fda7d46be40c655a617e0b441d52cd2c
gui3 <gui.silvent@gmail.com>
2022-11-11 17:39:40 +0100

    bigfix docker_run script

--- s_docker_run.sh
+++ s_docker_run.sh
@@ -3,12 +3,11 @@
 # this script
 # runs the docker image in a new container
 
-cd $(dirname "$0") # goto this file directory
-
-cd ../..
 docker run \
     --detach \
-    --env-file ./server/.env \
     --publish 8080:8080 \
     --name songbook_dev \
     songbook:dev
+
+
+    #--env-file ./server/.env \
