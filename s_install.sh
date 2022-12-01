05d248674d665cdd54a6354118d62b2c9dd0306a
gui3 <gui.silvent@gmail.com>
2022-12-01 10:15:31 +0100

    bugfix

--- s_install.sh
+++ s_install.sh
@@ -7,12 +7,12 @@ npm run build
 npm install
 cd ..
 
-echo installing server
+echo "installing server"
 cd server
 NODE_ENV=development npm install
 npm run build
 
-echo setup server
+echo "setup server"
 npm run knex migrate:latest
 
 
